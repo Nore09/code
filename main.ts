51b1db9cfa36150a2901f86c870afa5c6e20a66b
Nore09 <124691842+Nore09@users.noreply.github.com>
2023-05-15 14:14:09 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,27 +1,7 @@
-let start = 0
 let code = 0
 basic.forever(function () {
-    if (true) {
-        code += 1
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up)) {
-        start = 0
-    }
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
-        start = 0
-    }
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
-        start = 0
-    }
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) {
-        start = 0
-    }
-})
-basic.forever(function () {
     if (code == 0 && input.buttonIsPressed(Button.A)) {
-        code += 1
+        code = 1
     }
     if (code == 1 && input.buttonIsPressed(Button.B)) {
         code += 1
@@ -39,5 +19,67 @@ basic.forever(function () {
 basic.forever(function () {
     if (code == 0) {
         basic.showString("c")
+    }
+    if (code == 1) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    }
+    if (code == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+    }
+    if (code == 3) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # . .
+            `)
+    }
+    if (code == 4) {
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+    }
+    if (code == 5) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (code == 1 && (input.buttonIsPressed(Button.B) || input.logoIsPressed())) {
+        basic.showIcon(IconNames.No)
+    }
+    if (code == 2 && (input.buttonIsPressed(Button.A) || input.logoIsPressed())) {
+        basic.showIcon(IconNames.No)
+    }
+    if (code == 3 && (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.A))) {
+        basic.showIcon(IconNames.No)
+    }
+    if (code == 4 && (input.buttonIsPressed(Button.B) || input.logoIsPressed())) {
+        basic.showIcon(IconNames.No)
+    }
+    if (code == 5 && (input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.A))) {
+        basic.showIcon(IconNames.No)
     }
 })
